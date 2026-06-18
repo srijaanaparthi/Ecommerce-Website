ea18e1eb45b267d5bc0fb07874f8fb0a8ac6f176
srijaanaparthi <srijaanaparthi@gmail.com>
2026-06-18 13:08:11 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 
 import './App.css';
-import ProductCard from './pages/productCard';
-function App() {
+import ProductCard from './pages/ProductCard';
+import { Routes,Route,useNavigate } from 'react-router-dom';
+function Login() {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate('/products');
+  };
   return (
     <div className="App">
       <h1>Welcome to our E-commerce Website!</h1>
+      <label htmlFor='email'>email</label>
       <input type="email" placeholder="Enter your email" />
       <section>
+        <label htmlFor='password'>password</label>
       <input type="password" placeholder="Enter your password" />
       </section>
-      <button >Login</button>
-  
-      {/* Add your components and routes here */}
+      <button onClick={handleLogin}>Login</button>
+
     </div>
+  );
+}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/products" element={<ProductCard />} />
+    </Routes>
   );
 }
 export default App;
